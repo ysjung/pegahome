772bec80b4a70d7fe45e98ea45b7563f21c50ec8
ysjung <jungys85@gmail.com>
2011-10-06 16:06:41 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pegahome::Application.config.secret_token = '3b08e8d920443672fbc10b5758f5d0d077f4b9f8dfef93f7776a6de73dbac8149af12a11adfcbd823d364c6343c80b7612fae97b875e21165b472a21b295af8a'
